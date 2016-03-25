af55724f2ae5baae691d7f2c7f3510d64c3d5ff6
Nogamara <Nogamara@users.noreply.github.com>
2016-03-25 19:03:17 +0100

    Tweak new-heroes view

--- antihero/app/controllers/welcome_controller.rb
+++ antihero/app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def title
+    agent = Mechanize.new
+    agent.get(params[:url])
+
+    s = agent.page.title
+    s = s.sub(/ - [\w ]+ - Marvel Heroes - Items Base$/, "")
+
+    render plain: s
+
+  end
 end
